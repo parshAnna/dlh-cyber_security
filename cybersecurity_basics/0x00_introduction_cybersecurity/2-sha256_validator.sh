f8a01d0aeff320b66993a07b56fe60f7abc54b18
parshAnna <daneshpashana@gmail.com>
2026-05-05 20:13:30 +0000

    Fix formatting with printf

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-printf "  
-" "$2" "$1" | sha256sum -c --status 2>/dev/null && echo ok || echo invalid
+printf "  \n" "$2" "$1" | sha256sum -c --status 2>/dev/null && echo ok || echo invalid
