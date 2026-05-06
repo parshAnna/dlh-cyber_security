c69e3315fc2d1cc0764e2338fa985124df23808e
parshAnna <parshAnna@gmail.com>
2026-05-06 09:38:04 +0000

    Fix validator

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-HASH=$(sha256sum "$1" | awk '{print $1}')
-
-if [ "$HASH" = "$2" ]
-then
-	echo ok
-else
-	echo invalid
-fi
+[ "$(sha256sum "$1" | cut -d' ' -f1)" = "$2" ] && echo ok || echo invalid
