3e8d4646d653c94b413a41667da83b7c0ef2b91c
parshAnna <daneshparshana@gmail.com>
2026-05-26 17:29:39 +0000

    Fix subfinder command options

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | tee >(while read host; do ip=$(dig +short $host | grep -m1 '^[0-9]'); [ -n "$ip" ] && echo "$host,$ip"; done > $1.txt)
+subfinder -d $1 -silent -o $1.txt -nW -oI
